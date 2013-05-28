9a9d0d7003f4072e33abe7b03f699689d0478370
darkFunction <sam@darkfunction.com>
2013-05-28 13:19:26 +0100

    Return key classes from model builder

--- ObjC2yUML/DFModelBuilder.h
+++ ObjC2yUML/DFModelBuilder.h
@@ -9,6 +9,16 @@
 #import <Foundation/Foundation.h>
 #import "DFClangParserDelegate.h"
 
+@protocol DFModelBuilderDelegate;
+
 @interface DFModelBuilder : NSObject <DFClangParserDelegate>
+@property (nonatomic, weak) id<DFModelBuilderDelegate> delegate;
 - (id)initWithFilenames:(NSArray*)fileNames;
+
+// Return classes for which we have found an @implementation
+- (NSMutableDictionary*)keyClasses;
+@end
+
+@protocol DFModelBuilderDelegate <NSObject>
+- (void)modelCompleted:(DFModelBuilder*)modelBuilder;
 @end
