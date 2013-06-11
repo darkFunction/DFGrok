7fca9024f556f4e1eeed6f7e1d9d9ca02a4adfc2
Samuel Taylor <sam.taylor@corp.badoo.com>
2013-06-11 21:51:01 +0100

    Restored prev test filenames

--- DFGrok/main.m
+++ DFGrok/main.m
@@ -49,28 +49,12 @@ int main(int argc, char * argv[]) {
     @autoreleasepool {
         
         NSArray* filenames = [NSArray arrayWithObjects:
-//                              @"/Users/samtaylor/Work/Badoo/Classes/Comms/Badoo.pb.m",
-                              
-//                              @"/Users/samtaylor/Work/Badoo/Classes/BMAMessagesController.m",
-//                              @"/Users/samtaylor/Work/Badoo/Classes/BMAMessagesDataProvider.m",
-//                              @"/Users/samtaylor/Work/Badoo/Classes/BMAMessagesUserData.m",
-//                              @"/Users/samtaylor/Work/Badoo/Classes/BMAMessagesUserListModel.m",
-//                              @"/Users/samtaylor/Work/Badoo/Classes/BMAMessagesFilterView.m",
-//                              @"/Users/samtaylor/Work/Badoo/Classes/BMAMessagesUserCell.m",
-//                              @"/Users/samtaylor/Work/Badoo/Classes/BMAModelContainer.m",
-//                              @"/Users/samtaylor/Work/Badoo/Classes/BMAChatMessageModel.m",
-//                              @"/Users/samtaylor/Work/Badoo/Classes/BMAMessagesUserListModel.m",
-//                              @"/Users/samtaylor/Work/Badoo/Classes/BMAPersonStatusModel.m",
-//                              @"/Users/samtaylor/Work/Badoo/Classes/BMAPersonModel.m",
-                              
-                              @"/Users/samtaylor/Work/Badoo/Classes/BMAEncountersController.m",
-                              @"/Users/samtaylor/Work/Badoo/Classes/BMAEncountersDataProvider.m",
-                              @"/Users/samtaylor/Work/Badoo/Classes/BMAEncountersModel_v2.m",
-                              @"/Users/samtaylor/Work/Badoo/Classes/BMAClientPictureModel.m",
-                              @"/Users/samtaylor/Work/Badoo/Classes/BMAClientVoteResponseModel.m",
-                              
-                              
-
+                              @"/Users/samtaylor/Projects/ObjC2yUML/UMLTestProject/UMLTestProject/Classes/DFDemoController.m",
+                              @"/Users/samtaylor/Projects/ObjC2yUML/UMLTestProject/UMLTestProject/Classes/DFDataModelContainer.m",
+                              @"/Users/samtaylor/Projects/ObjC2yUML/UMLTestProject/UMLTestProject/Classes/DFDataModel.m",
+                              @"/Users/samtaylor/Projects/ObjC2yUML/UMLTestProject/UMLTestProject/Classes/DFDemoDataSource.m",
+                              @"/Users/samtaylor/Projects/ObjC2yUML/UMLTestProject/UMLTestProject/Classes/DFDemoDataModelOne.m",
+                              @"/Users/samtaylor/Projects/ObjC2yUML/UMLTestProject/UMLTestProject/Classes/DFDemoDataModelTwo.m",
                               nil];
 
         DFModelBuilder* modelBuilder = [[DFModelBuilder alloc] initWithFilenames:filenames];
